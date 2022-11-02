6d623464b6d0b6deb7cc2ffb07798228ee520cff
Temur Mikava <temsonmikava@gmail.com>
2022-11-02 15:36:21 +0400

    Add database context and connection string

---
+++ Humans/HumansAPI/Data/SqlDbContext.cs
@@ -0,0 +1,31 @@
+﻿using HumansAPI.Models.Domain;
+using Microsoft.EntityFrameworkCore;
+
+namespace HumansAPI.Data
+{
+    public class SqlDbContext : DbContext
+    {
+        public SqlDbContext(DbContextOptions<SqlDbContext> options) : base(options)
+        {
+
+        }
+
+        DbSet<Human> Humans { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            builder.Entity<HumanConnection>()
+                .HasOne(x => x.FirstHuman)
+                .WithMany(x => x.Connections)
+                .HasForeignKey(x => x.FirstHumanId)
+                .OnDelete(DeleteBehavior.Cascade);
+            builder.Entity<HumanConnection>()
+                .HasOne(x => x.SecondHuman)
+                .WithMany()
+                .HasForeignKey(x => x.SecondHumanId)
+                .OnDelete(DeleteBehavior.NoAction);
+                
+        }
+
+    }
+}
--- Humans/HumansAPI/Program.cs
+++ Humans/HumansAPI/Program.cs
@@ -1,3 +1,6 @@
+using HumansAPI.Data;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +9,7 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddDbContext<SqlDbContext>(options => options.UseSqlServer(builder.Configuration.GetConnectionString("HumansConnectionString")));
 
 var app = builder.Build();
 
