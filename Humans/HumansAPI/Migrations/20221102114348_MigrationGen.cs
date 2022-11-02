34715ac84b2de9bbd25b0f5d9ae165ef03fa279d
Temur Mikava <temsonmikava@gmail.com>
2022-11-02 15:47:02 +0400

    Generate migration files

---
+++ Humans/HumansAPI/Migrations/20221102114348_MigrationGen.cs
@@ -0,0 +1,131 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace HumansAPI.Migrations
+{
+    public partial class MigrationGen : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "City",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_City", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Humans",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    FirstName = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    LastName = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Gender = table.Column<byte>(type: "tinyint", nullable: false),
+                    PersonalNumber = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    DateOfBirth = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    CityId = table.Column<int>(type: "int", nullable: true),
+                    PhotoPath = table.Column<string>(type: "nvarchar(max)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Humans", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Humans_City_CityId",
+                        column: x => x.CityId,
+                        principalTable: "City",
+                        principalColumn: "Id");
+                });
+
+            migrationBuilder.CreateTable(
+                name: "HumanConnection",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    FirstHumanId = table.Column<int>(type: "int", nullable: false),
+                    SecondHumanId = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_HumanConnection", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_HumanConnection_Humans_FirstHumanId",
+                        column: x => x.FirstHumanId,
+                        principalTable: "Humans",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_HumanConnection_Humans_SecondHumanId",
+                        column: x => x.SecondHumanId,
+                        principalTable: "Humans",
+                        principalColumn: "Id");
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Phone",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    PhoneNumber = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Type = table.Column<byte>(type: "tinyint", nullable: false),
+                    HumanId = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Phone", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Phone_Humans_HumanId",
+                        column: x => x.HumanId,
+                        principalTable: "Humans",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_HumanConnection_FirstHumanId",
+                table: "HumanConnection",
+                column: "FirstHumanId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_HumanConnection_SecondHumanId",
+                table: "HumanConnection",
+                column: "SecondHumanId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Humans_CityId",
+                table: "Humans",
+                column: "CityId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Phone_HumanId",
+                table: "Phone",
+                column: "HumanId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "HumanConnection");
+
+            migrationBuilder.DropTable(
+                name: "Phone");
+
+            migrationBuilder.DropTable(
+                name: "Humans");
+
+            migrationBuilder.DropTable(
+                name: "City");
+        }
+    }
+}
