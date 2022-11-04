d8f1cdaafb88b51c65b985ef804581758331e0f7
Temur Mikava <temsonmikava@gmail.com>
2022-11-04 18:49:54 +0400

    Add validations to phones controller

---
+++ Humans/HumansAPI/Validators/AddPhoneRequestValidator.cs
@@ -0,0 +1,27 @@
+﻿using FluentValidation;
+using HumansAPI.DTOs;
+using HumansAPI.Models.Domain;
+using HumansAPI.Repositories;
+
+namespace HumansAPI.Validators
+{
+    public class AddPhoneRequestValidator:AbstractValidator<AddPhoneRequest>
+    {
+        private readonly IRepository<Human> humans;
+
+        public AddPhoneRequestValidator(IRepository<Human> humans)
+        {
+            this.humans = humans;
+
+            RuleFor(x => x.PhoneNumber).NotEmpty().Length(4, 50);
+            RuleFor(x => x.Type).IsInEnum();
+            RuleFor(x => x.HumanId).Must(IfExistHuman);
+            
+        }
+
+        private bool IfExistHuman(int humanId)
+        {
+            return humans.CheckAsync(x => x.Id == humanId).Result;
+        }
+    }
+}
---
+++ Humans/HumansAPI/Validators/UpdatePhoneRequestValidator.cs
@@ -0,0 +1,24 @@
+﻿using FluentValidation;
+using HumansAPI.DTOs;
+using HumansAPI.Models.Domain;
+using HumansAPI.Repositories;
+
+namespace HumansAPI.Validators
+{
+    public class UpdatePhoneRequestValidator:AbstractValidator<UpdatePhoneRequest>
+    {
+        private readonly IRepository<Phone> phones;
+
+        public UpdatePhoneRequestValidator(IRepository<Phone> phones)
+        {
+            this.phones = phones;
+            RuleFor(x => x.Id).Must(IfExistPhone);
+            RuleFor(x => x.Type).IsInEnum();
+            RuleFor(x => x.PhoneNumber).NotEmpty().Length(4, 50);
+        }
+        private bool IfExistPhone(int id)
+        {
+            return phones.CheckAsync(x => x.Id == id).Result;
+        }
+    }
+}
